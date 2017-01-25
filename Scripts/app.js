98fe56447547ad1adb71adef0cbc438afb2517cd
JCannon1 <jessecannon1@hotmail.com>
2017-01-25 16:39:06 -0500

    added personal mission paragraph

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,33 @@
 /* javascript lives here */
 
-conosle.log("App Started");
+console.log("App Started");
+
+// declare/ initialize firstHeading variable 
+var firstHeading = document.getElementById("firstHeading");
+
+
+console.log(firstHeading.textContent);
+
+var FirstParagraph = document.getElementById("FirstParagraph");
+
+var myContent = "Hello, my name is Jesse Cannon and my goal of this portfolio site is to show you all of my web design skills I have acquired from my time at Georgian College. My Projects page will display all of the top pieces of work I have done so far for visuals. While the Contact page will ask you for your contact information. I hope my page is of great use to you. Jesse Cannon.";
+
+FirstParagraph.textContent = myContent;
+
+var SecondParagraph = document.getElementById("SecondParagraph");
+var myHTMLContent = "<h2>About Me</h2>"
+                    + " <p></p>"
+
+SecondParagraph.innerHTML = myHTMLContent;
+
+
+// create a reference to the button on the page (index.html)
+var clickMeButton = document.getElementById("clickMeButton");
+
+// add an event listener - for the click event and call the Click function
+clickMeButton.addEventListener("click", Click);
+
+// click function - used as an event handler
+function Click() {
+console.log("clicked");
+}
